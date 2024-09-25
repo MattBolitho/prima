6f5004ddc521089f924176f4608ec6764c4df804
MattBolitho <matt.bolitho.software@gmail.com>
2024-09-25 23:27:18 +0100

    Adds default initialization functions

--- rust/prima_sys/src/lib.rs
+++ rust/prima_sys/src/lib.rs
@@ -67,3 +67,88 @@ impl Default for prima_result_t {
         }
     }
 }
+
+/// Gets a new, initialized problem with the given number of variables.
+/// `prima_init_problem` will have been called on the return value.
+///
+/// This function is exported on top of the C bindings because the PRIMA API
+/// uses an uninitialized struct function call pattern to to initialize problem
+/// structs:
+///
+/// ```c
+/// const int n = 2;
+/// prima_problem_t problem;
+/// prima_init_problem(&problem, n);
+/// ```
+///
+/// In Rust, structs are always fully initialized, so we provide this function
+/// as convenience to correctly initialize a problem whilst avoiding any
+/// potential undefined behavior and/or excessive repetition.
+///
+/// ## Example
+///
+/// ```rust///
+/// let number_of_variables: i32 = 2;
+/// let mut problem = prima_sys::new_problem(number_of_variables);
+/// // Add your further problem initialization here...
+/// ```
+///
+/// ## Parameters
+/// - `number_of_variables`: The number of variables in the problem.
+///
+/// ## Returns
+/// A new, initialized `prima_problem_t` instance..
+pub fn new_problem(number_of_variables: i32) -> prima_problem_t {
+    let mut problem = Default::default();
+    let problem_ptr = std::ptr::addr_of_mut!(problem);
+    unsafe {
+        prima_init_problem(problem_ptr, number_of_variables as i32);
+    }
+    problem
+}
+
+/// Gets a new, initialized options struct. `prima_init_options` will have been
+/// called on the return value.
+///
+/// This function is exported on top of the C bindings because the PRIMA API
+/// uses an uninitialized struct function call pattern to to initialize problem
+/// structs:
+///
+/// ```c
+/// prima_options_t options;
+/// prima_init_options(&options);
+/// ```
+///
+/// In Rust, structs are always fully initialized, so we provide this function
+/// as convenience to correctly initialize options whilst avoiding any
+/// potential undefined behavior and/or excessive repetition.
+///
+/// ## Example
+///
+/// ```rust
+/// let options = prima_sys::new_options();
+/// // Add your further options initialization here...
+/// ```
+///
+/// ## Returns
+/// A new, initialized `prima_options_t` instance.
+pub fn new_options() -> prima_options_t {
+    let mut options = Default::default();
+    let options_ptr = std::ptr::addr_of_mut!(options);
+    unsafe {
+        prima_init_options(options_ptr);
+    }
+    options
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_problem_contains_expected_number_of_variables() {
+        let number_of_variables: i32 = 2;
+        let problem = new_problem(number_of_variables);
+        assert_eq!(number_of_variables, problem.n);
+    }
+}
